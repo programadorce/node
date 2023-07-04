527a8d271c7004fce1c470c6a60f2dfb3aab3ec6
Thiago Soares <programadorceara@gmail.com>
2023-07-04 10:38:30 -0300

    Como criar o put na API

--- README.md
+++ README.md
@@ -137,3 +137,41 @@ app.post("/contatos",(req,res) =>{
 app.listen(3000,() => {
     console.log("Servidor iniciado na porta 3000");
 });
+
+
+#Como criar o put na API
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const contatos = ["Thiago", "Camila", "Maria Thaís"];
+
+app.get("/", (req, res) => {
+    return res.json(contatos);
+});
+
+app.get("/contato/:id", (req, res) => {
+    const { id } = req.params;
+    return res.json({
+        nome: contatos[id]
+    })
+});
+
+app.post("/contatos", (req, res) => {
+    const { nome } = req.body;
+    contatos.push(nome);
+    return res.json(contatos)
+});
+
+app.put("/contatos/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+    contatos[id] = nome;
+    return res.json(contatos);
+});
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000");
+});
--- app.js
+++ app.js
@@ -4,27 +4,32 @@ const app = express();
 
 app.use(express.json());
 
-const contatos = ["Thiago","Camila","Maria Thaís"];
+const contatos = ["Thiago", "Camila", "Maria Thaís"];
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     return res.json(contatos);
 });
 
-app.get("/contato/:id", (req,res) =>{
-    const {id}= req.params;
+app.get("/contato/:id", (req, res) => {
+    const { id } = req.params;
     return res.json({
         nome: contatos[id]
     })
 });
 
-app.post("/contatos",(req,res) =>{
-    const {nome} = req.body;
-
+app.post("/contatos", (req, res) => {
+    const { nome } = req.body;
     contatos.push(nome);
-
     return res.json(contatos)
-})
+});
 
-app.listen(3000,() => {
+app.put("/contatos/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+    contatos[id] = nome;
+    return res.json(contatos);
+});
+
+app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000");
 });
